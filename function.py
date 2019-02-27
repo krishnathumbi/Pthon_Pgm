f001fb084e72a26f4ae8d8faa93b17ca35d8448a
krishnathumbi <thumbikrishna18@gmail.com>
2019-02-27 10:57:56 +0530

    FTN + & -

--- function.py
+++ function.py
@@ -1,6 +1,13 @@
 def addition(x,y):
     z=x+y
     print("Sum of ",x," & ",y," : ",z)
+
+def subtraction(x,y):
+    z=x-y
+    print("Difference of ",x," & ",y," : ",z)
+
 a=250
 b=450
+
 addition(a,b)
+subtraction(b,a)
