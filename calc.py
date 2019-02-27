e9e491713ed1d9f2c100a40ef0940782959dd26a
krishnathumbi <thumbikrishna18@gmail.com>
2019-02-27 11:09:23 +0530

    Module + & - 1st

---
+++ calc.py
@@ -0,0 +1,7 @@
+def addition(x,y):
+    z=x+y
+    print("Sum of ",x," & ",y," : ",z)
+
+def subtraction(x,y):
+    z=x-y
+    print("Difference of ",x," & ",y," : ",z)
---
+++ modules.py
@@ -0,0 +1,6 @@
+import calc
+a=250
+b=450
+
+calc.addition(a,b)
+calc.subtraction(b,a)
